3d6eaed40adb26515dbe3e3c45741632fe837086
Joe Netti <joe@netti.dev>
2023-01-13 16:25:08 -0700

    constructors: 32, 64, and constrained

--- bloom.go
+++ bloom.go
@@ -1,5 +1,10 @@
 package bloom
 
+import (
+	"errors"
+	"math"
+)
+
 type Bloomer interface {
 	// put in bloom. Bool if successful
 	putStr(string) (bool)
@@ -9,7 +14,7 @@ type Bloomer interface {
 	existsStr(string) (bool, float64);
 	existsBytes([]byte) (bool, float64);
 
-	// returns accuracy: (1 - probability of false positive)
+	// returns accuracy: (1 - probability of exist returning a false positive at current n entries)
 	accuracy() float64;
 }
 
@@ -23,9 +28,72 @@ type Bloom struct {
 	// number of bytes. only 32 or 64 allowed
 	size int
 
-	// min_accuracy (default is 0)
-	min_accuracy float64
+	// optional, the maximum allowed false positive rate until no more entries accepted
+	maxFalsePositiveRate *float64
 
 	// optional, maximum number of entries allowed
 	cap *int 
+}
+
+// Bloom type constructors
+
+func NewBloom32() *Bloom {
+	return &Bloom{
+		n: 0,
+		bs: make([]byte, 32),
+		size: 32,
+		maxFalsePositiveRate: nil,  // don't care about accuracy unless specified
+		cap: nil,        			// don't care about capacity unless specified
+	}
+}
+
+func NewBloom64() *Bloom {
+	return &Bloom{
+		n: 0,
+		bs: make([]byte, 64),
+		size: 64,
+		maxFalsePositiveRate: nil,  // don't care about accuracy unless specified
+		cap: nil,        			// don't care about capacity unless specified
+	}
+}
+
+func NewBloom() *Bloom {
+	return NewBloom64()
+}
+
+// cap: max number of entries 
+// min_accuracy: max allowed
+// ex: if maxFalsePositiveRate is 0.1 then 10% chance of false positive when capacity is full
+func NewBloomConstrain(cap *int, maxFalsePositiveRate *float64) (*Bloom, error) {
+	// use 64
+	b := NewBloom64()
+
+	if maxFalsePositiveRate != nil {
+		if *maxFalsePositiveRate < 0 {
+			return nil, errors.New("false positive rate cannot be negative")
+		}
+		b.maxFalsePositiveRate = maxFalsePositiveRate
+	}
+	if cap != nil {
+		if *cap < 0 {
+			return nil, errors.New("capacity cannot be negative") 
+		}
+		b.cap = cap
+	}
+	if cap == nil || maxFalsePositiveRate == nil {
+		// do not need to check for compatibility of constraints. 
+		return b, nil
+	}
+
+	// check if the capacity and min_accuracy are even possible together with 64 byte size
+	// equation: 1 - (1/m)^n where m is bits and n is entries
+	// math here: https://brilliant.org/wiki/bloom-filter/
+	base := 1 - float64(1)/float64(512)
+	calcMaxFalsePositiveRate := math.Pow(base, float64(*cap))
+	if calcMaxFalsePositiveRate > *maxFalsePositiveRate  {
+		// if the maximum calculated false positive rate is greater user inputed allowed false positive rate, fail.
+		return nil, errors.New("false positive rate will be higher in full bloom filter than the maxFalsePositiveRate provided")
+	}
+
+	return b, nil
 }
